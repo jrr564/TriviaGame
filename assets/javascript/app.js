d4b86cd8f133fb2bb6803ca4f36e627ec8e6af3d
admin <admin@vraney-mbp.thelamplighterschool.org>
2017-11-28 23:18:04 -0600

    stuck

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,33 @@
 //set up timeout event; ends after 30 second
-var rightAnswer = 0;
+
 var wrongAnswer = 0;
 var noAnswer = 0;
+var rightAnswer = 0;
 
 
-setTimeout(timeUp, 1000 * 5);
+
+setTimeout(timeUp, 1000 * 3);
 
 //time up screen
 function timeUp() {
-//to say time's up
-	console.log("time up");
+	if(document.getElementsByClassName('correct').checked) {
+	  rightAnswer++
+	}else if(document.getElementById('wrong').checked) {
+	  wrongAnswer++
+	};
+	// $(".correct:checked").each(function(){
+	// 	rightAnswer+=parseInt($(this).val())
+	// 	});
+	// $("input[name=optradio]").val(rightAnswer);
+	$("#correctKey").text("Correct: "+rightAnswer+".");
 	$("#timer").text("Time's up sucka");
-	$("#container").text("");
-	$("#correct").text("Correct: "+rightAnswer+"");
 	$("#wrong").text("Wrong: "+wrongAnswer+"");
-	$("#unanswered").text("Unanswered: "+noAnswer+"");
+	$("#unanswered").text("Unanswered: "+noAnswer+".");
+	$(".question").empty("");
 //answers right
 //answer wrong
 };
 
-function questionOne() {
-	var correct = ".correct";
-	var choice = 
-
-	if (choice === correct) {
-		rightAnswer++;
-	};
-	else {
-		wrongAnswer++:
-	}
-
-}
-
-function questionTwo() {
-	
-}
-
-function questionThree() {
-	
-}
-
-function questionOne() {
-	
-}
-
-function questionOne() {
-	
+function startUp() {
+	$(".question").show();
 }
